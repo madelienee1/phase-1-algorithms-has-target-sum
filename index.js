98c03c7cd6be99a56c226e35ffcab8b083061b54
Madeliene <madelienestiller@gmail.com>
2023-12-04 13:00:37 +1000

    has target sum lab complete not passing all tests

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let number = 0; number < array.length; number++) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[number] + array[i] === target) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 /* 
-  Write the Big O time complexity of your function here
+The time complexity of the function is O(n2). It has 2 nested 'for' loops.
+
 */
 
 /* 
-  Add your pseudocode here
-*/
+Create a function called hasTargetSum that takes 2 parameters - array of integers and target integer
+Output needs to be a boolean - true if pair adds up to target, otherwise false.
+For each element in array as current element
+for each element in array starting from the next element, as next element
+If sum of current element and next element equals target then return true. Otherwise return false */
 
 /*
-  Add written explanation of your solution here
+  The function checks all combinations of 2 numbers in the array to see if they add up to the target.
+  It it does it returns true. If it doesn't it returns false./
 */
 
 // You can run `node index.js` to view these console logs
